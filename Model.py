de5d78bb6526ae87b2e045b4ddfc90b0691ed6c6
Harsha <ashharsha1999@gmail.com>
2020-06-29 07:35:22 +0530

    Rectified errors and added SemiUNnet Model

--- Model.py
+++ Model.py
@@ -1,14 +1,14 @@
-from torch import nn
+import torch.nn as nn
 import pretrainedmodels
 import torch.nn.functional as F
 
 
 class Resnet34(nn.Module):
-    def __init__(self, model_name = 'resnext50_32x4d'):
+    def __init__(self):
         super().__init__()
 
         self.base_model = pretrainedmodels.__dict__['resnet34'](pretrained  = None)
-    
+
         num_ftrs = self.base_model.last_linear.in_features
         self.final = nn.Sequential(
                                 nn.Linear(num_ftrs, 32),
@@ -17,15 +17,83 @@ class Resnet34(nn.Module):
                                 nn.ReLU(inplace = True),
                                 nn.Linear(16,8),
                                 nn.ReLU(inplace = True),
-                                nn.Linear(8,1)
+                                nn.Linear(8,1),
+                                nn.Sigmoid()
             )
         self.base_model.avgpool = None
         self.base_model.last_linear = None
 
     def forward(self, x):
         bs,_,_,_ = x.shape
-        x = self.model.features(x)
+        x = self.base_model.features(x)
         x = F.adaptive_avg_pool2d(x,1).reshape(bs,-1)
+        x = self.final(x)
+
+        return x
+
+class SemiUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        ## Conv Layers for Feature Extraction
+        self.c1 = nn.Conv2d(3, 64)
+        self.b1 = nn.BatchNorm2d()
+        self.c2 = nn.Conv2d(64, 64)
+        self.b2 = nn.BatchNorm2d()
+
+        self.c3 = nn.Conv2d(128, 128)
+        self.b3 = nn.BatchNorm2d()
+        self.c4 = nn.Conv2d(128,128)
+        self.b4 = nn.BatchNorm2d()
+
+        self.d1 = nn.MaxPool2d(2)
+        self.c5 = nn.Conv2d(128,256)
+
+        self.d2 = nn.MaxPool2d(2)
+        self.c6 = nn.Conv2d(512)
+
+        ## FC Layers for Classification
+        self.final = nn.Sequential(
+                                nn.Linear(512, 32),
+                                nn.ReLU(inplace = True),
+                                nn.Linear(32,16),
+                                nn.ReLU(inplace = True),
+                                nn.Linear(16,8),
+                                nn.ReLU(inplace = True),
+                                nn.Linear(8,1),
+                                nn.Sigmoid()
+            )
+
+
+    def forward(self,x):
+
+        bs,_,_,_ = x.shape
+
+        x  = self.c1(x)
+        x  = self.b1(x)
+        x  = F.relu(x)
+
+        x  = self.c2(x)
+        x  = self.b2(x)
+        x  = F.relu(x)
+
+        x  = self.c3(x)
+        x  = self.b4(x)
+        x  = F.relu(x)
+
+        x  = self.c4(x)
+        x  = self.b4(x)
+        x  = F.relu(x)
+
+        x  = self.d1(x)
+        x  = self.c5(x)
+        x  = F.relu(x)
+
+        x  = self.d2(x)
+        x  = self.c6(x)
+
+        x = F.adaptive_avg_pool2d(x,1).reshape(bs,-1)
+
         x = self.final(x)
 
         return x
